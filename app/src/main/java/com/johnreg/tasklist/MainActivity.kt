18c48e800ff70ccd4b0eb9e7465fdb73fb20c5ab
John Reg <johnreginaldjao@gmail.com>
2024-01-19 03:04:32 +0800

    added task-dialog and setOnClickListener FloatingActionButton

--- app/src/main/java/com/johnreg/tasklist/MainActivity.kt
+++ app/src/main/java/com/johnreg/tasklist/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.johnreg.tasklist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.tabs.TabLayoutMediator
 import com.johnreg.tasklist.databinding.ActivityMainBinding
+import com.johnreg.tasklist.databinding.DialogAddTaskBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +25,20 @@ class MainActivity : AppCompatActivity() {
         TabLayoutMediator(binding.tabs, binding.pager) { tab, _ ->
             tab.text = "Tasks"
         }.attach()
+
+        binding.fab.setOnClickListener { showAddTaskDialog() }
+    }
+
+    private fun showAddTaskDialog() {
+        val dialogBinding = DialogAddTaskBinding.inflate(layoutInflater)
+        MaterialAlertDialogBuilder(this)
+            .setTitle("Add new task")
+            .setView(dialogBinding.root)
+            .setPositiveButton("Save") { _, _ ->
+                Toast.makeText(this, "Your task is: ${dialogBinding.editText.text}", Toast.LENGTH_LONG).show()
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
     }
 
     inner class PagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
