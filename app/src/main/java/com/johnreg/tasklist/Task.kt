8b1ca0aa97860558ffbc2cb1cd499e13ca3f304d
John Reg <johnreginaldjao@gmail.com>
2024-01-23 21:07:55 +0800

    added Task Entity

---
+++ app/src/main/java/com/johnreg/tasklist/Task.kt
@@ -0,0 +1,12 @@
+package com.johnreg.tasklist
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Task(
+    @PrimaryKey(autoGenerate = true) val taskId: Int = 0,
+    val title: String,
+    val description: String? = null,
+    val isStarred: Boolean = false
+)
