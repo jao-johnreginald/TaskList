b175762df627602474f998f18702d79a368a77e9
John Reg <johnreginaldjao@gmail.com>
2024-01-26 00:48:04 +0800

    modified TaskListDatabase

--- app/src/main/java/com/johnreg/tasklist/data/TaskListDatabase.kt
+++ app/src/main/java/com/johnreg/tasklist/data/TaskListDatabase.kt
@@ -7,4 +7,6 @@ import com.johnreg.tasklist.data.Task
 @Database(entities = [Task::class], version = 1)
 abstract class TaskListDatabase : RoomDatabase() {
 
+    abstract fun getTaskDao() : TaskDao
+
 }
