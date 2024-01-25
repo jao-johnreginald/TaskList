aeff72d62d037bbab26d98bbbab946c2364abb1b
John Reg <johnreginaldjao@gmail.com>
2024-01-25 23:54:40 +0800

    modified TaskDao

--- app/src/main/java/com/johnreg/tasklist/data/TaskDao.kt
+++ app/src/main/java/com/johnreg/tasklist/data/TaskDao.kt
@@ -1,4 +1,24 @@
 package com.johnreg.tasklist.data
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
 interface TaskDao {
+
+    @Insert
+    fun createTask(task: Task)
+
+    @Query("SELECT * FROM task")
+    fun getAllTasks(): List<Task>
+
+    @Update
+    fun updateTask(task: Task)
+
+    @Delete
+    fun deleteTask(task: Task)
+
 }
