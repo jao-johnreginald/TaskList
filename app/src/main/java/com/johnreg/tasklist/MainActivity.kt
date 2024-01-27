05d2dadeb1d652612c4f4474fc22eca6792315ba
John Reg <johnreginaldjao@gmail.com>
2024-01-27 11:33:20 +0800

    deleted thread | refactored class fields | added BottomSheetDialog

--- app/src/main/java/com/johnreg/tasklist/MainActivity.kt
+++ app/src/main/java/com/johnreg/tasklist/MainActivity.kt
@@ -1,22 +1,24 @@
 package com.johnreg.tasklist
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.tabs.TabLayoutMediator
-import com.johnreg.tasklist.data.Task
+import com.johnreg.tasklist.data.TaskDao
 import com.johnreg.tasklist.data.TaskListDatabase
 import com.johnreg.tasklist.databinding.ActivityMainBinding
 import com.johnreg.tasklist.databinding.DialogAddTaskBinding
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var database: TaskListDatabase
+
+    private val taskDao: TaskDao by lazy { database.getTaskDao() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,34 +33,15 @@ class MainActivity : AppCompatActivity() {
 
         binding.fab.setOnClickListener { showAddTaskDialog() }
 
-        val database = TaskListDatabase.createDatabase(this)
-
-        val taskDao = database.getTaskDao()
-
-        thread {
-            taskDao.createTask(Task(
-                title = "hello task",
-                description = "description hello world",
-                isStarred = true
-            ))
-            val tasks = taskDao.getAllTasks()
-            runOnUiThread {
-                Toast.makeText(this, "Number of tasks: ${tasks.size}", Toast.LENGTH_LONG).show()
-            }
-        }
-
+        database = TaskListDatabase.createDatabase(this)
     }
 
     private fun showAddTaskDialog() {
         val dialogBinding = DialogAddTaskBinding.inflate(layoutInflater)
-        MaterialAlertDialogBuilder(this)
-            .setTitle("Add new task")
-            .setView(dialogBinding.root)
-            .setPositiveButton("Save") { _, _ ->
-                Toast.makeText(this, "Your task is: ${dialogBinding.editText.text}", Toast.LENGTH_LONG).show()
-            }
-            .setNegativeButton("Cancel", null)
-            .show()
+
+        val dialog = BottomSheetDialog(this)
+        dialog.setContentView(dialogBinding.root)
+        dialog.show()
     }
 
     inner class PagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
